c9c3603518ab19419eb8a9c9d1c07822096d7886
Andrei Mihale <48432916+andreimihale@users.noreply.github.com>
2022-12-05 18:55:47 +0200

    initialise repo with nextjs

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import * as NextImage from 'next/image';
+import '../styles/globals.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) =>
+    typeof props.src === 'string' ? (
+      <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+    ) : (
+      <OriginalNextImage {...props} unoptimized />
+    ),
+});
---
+++ stories/Home.stories.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Home from '../pages';
+
+export default {
+  title: 'Pages/Home',
+  component: Home,
+};
+
+export const HomePage = () => <Home />;
