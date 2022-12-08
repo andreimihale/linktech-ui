f625e9d2e12579993359dc2ae15438e0acd75c3a
Andrei Mihale <48432916+andreimihale@users.noreply.github.com>
2022-12-08 19:15:48 +0200

    create story for backdrop

---
+++ components/Utils/Backdrop/Backdrop.stories.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Button from '../../Inputs/Button';
+import Backdrop from './Backdrop';
+
+export default {
+  title: 'Components/Utils/Backdrop',
+  component: Backdrop,
+  argTypes: {},
+};
+
+const Template = (args) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <Button onClick={handleToggle}>Show backdrop</Button>
+      <Backdrop open={open} onClick={handleClose}>
+        This is the children of the backdrop
+      </Backdrop>
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
